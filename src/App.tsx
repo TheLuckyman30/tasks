a92d56d104fd07fd2c729493fce13ffa39d9763f
TheLuckyman30 <bnauta0@gmail.com>
2024-02-06 16:00:50 -0500

    Added Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Brandon Nauta
+                automatically reload. Brandon Nauta. Hello World
             </p>
         </div>
     );

b748fe4aa32282a1263e92251b5310ec8cf33585
TheLuckyman30 <bnauta0@gmail.com>
2024-02-18 18:47:51 -0500

    First two functions

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,7 +5,13 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
-    return numbers;
+    let firstAndLast: number[] = [];
+    if (numbers.length === 0) {
+        firstAndLast = [];
+    } else {
+        firstAndLast = [numbers[0], numbers[numbers.length - 1]];
+    }
+    return firstAndLast;
 }
 
 /**
@@ -13,7 +19,7 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    return numbers.map((number: number): number => number * 3);
 }
 
 /**

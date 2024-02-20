739251eeccf56a72949a1162c9a1b8003706dabb
TheLuckyman30 <bnauta0@gmail.com>
2024-02-19 22:44:11 -0500

    Shortened bookEndList

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,13 +5,9 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
-    let firstAndLast: number[] = [];
-    if (numbers.length === 0) {
-        firstAndLast = [];
-    } else {
-        firstAndLast = [numbers[0], numbers[numbers.length - 1]];
-    }
-    return firstAndLast;
+    return numbers.length === 0
+        ? []
+        : [numbers[0], numbers[numbers.length - 1]];
 }
 
 /**

44f2cc4162b2a1a559a2f412302b3cc3452a34b6
TheLuckyman30 <bnauta0@gmail.com>
2024-02-25 02:00:56 -0500

    Finished two more functions

--- src/objects.ts
+++ src/objects.ts
@@ -42,7 +42,14 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    if (question.type === "short_answer_question") {
+        return true;
+    }
+    return question.options.reduce(
+        (valid: boolean, option: string) =>
+            option === answer || valid ? (valid = true) : (valid = false),
+        false
+    );
 }
 
 /**
@@ -52,7 +59,7 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    return question.id.toString() + ": " + question.name.slice(0, 10);
 }
 
 /**

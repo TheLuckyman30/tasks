eae992fae29be01087f39d869ab36bd1a4a5d9e0
TheLuckyman30 <bnauta0@gmail.com>
2024-02-27 15:55:19 -0500

    Initial Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,5 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
-import { Button, Col, Container, Row } from "react-bootstrap";
-
-function App(): JSX.Element {
-    return (
-        <div>
-            <div className="App">
-                <header className="App-header">
-                    Hello World
-                    <br /> Author: Brandon Nauta
-                    <br /> Class: CISC275
-                </header>
-            </div>
-            <h1 className="App-welcomeHeader"> Welcome to my Website! </h1>
-            <div>
-                <Container className="App-columns">
-                    <Row>
-                        <Col className="App-columnLeft">
-                            <p style={{ fontSize: "30px" }}>
-                                <b>Favorite Videogame Series:</b>
-                            </p>
-                            <img
-                                src={require("./assets/images/Animal-Crossing-Logo.png")}
-                                className="App-image"
-                                alt="A picture of the Animal Crossing logo"
-                            />
-                            <p className="App-listHeader">
-                                <b>Why is it my favorite?</b>
-                            </p>
-                            <ul className="App-list">
-                                <li> It is very relaxing </li>
-                                <li> I love the progression </li>
-                                <li> And it is very fun </li>
-                            </ul>
-                            <div className="App-rectangles"></div>
-                        </Col>
-                        <Col className="App-columnRight">
-                            <p style={{ fontSize: "30px" }}>
-                                <b>Funny Button:</b>
-                            </p>
-                            <Button onClick={() => console.log("Hello World!")}>
-                                Log Hello World
-                            </Button>
-                            <div className="App-rectangles"></div>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-=======
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
@@ -74,7 +25,6 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
         </div>
     );
 }

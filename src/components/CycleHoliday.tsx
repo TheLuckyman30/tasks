6f7075479e78ea7bdc2bd995bda9fae0d70e033b
TheLuckyman30 <bnauta0@gmail.com>
2024-02-27 22:36:10 -0500

    Finished Task 8

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,35 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+const HOLIDAYS_IN_ORDER: Record<string, string> = {
+    "🎄": "💖",
+    "💖": "🐇",
+    "🐇": "🎃",
+    "🎃": "🦃",
+    "🦃": "🎄"
+};
+
+const HOLIDAYS_ALPHABETICALLY: Record<string, string> = {
+    "🎄": "🐇",
+    "🐇": "🎃",
+    "🎃": "🦃",
+    "🦃": "💖",
+    "💖": "🎄"
+};
+
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<string>("🎄");
+    return (
+        <div>
+            <div>Holiday: {holiday}</div>
+            <Button
+                onClick={() => setHoliday(HOLIDAYS_ALPHABETICALLY[holiday])}
+            >
+                Advance by Alphabet
+            </Button>
+            <Button onClick={() => setHoliday(HOLIDAYS_IN_ORDER[holiday])}>
+                Advance by Year
+            </Button>
+        </div>
+    );
 }
